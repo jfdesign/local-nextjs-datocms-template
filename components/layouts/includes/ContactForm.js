9d22af58bfa715e23fdb36dbdd0199a09a532fc7
John <jfdesign@gmail.com>
2022-07-20 13:50:07 -0500

    htmlfor

--- components/layouts/includes/ContactForm.js
+++ components/layouts/includes/ContactForm.js
@@ -8,17 +8,17 @@ export default function ContactForm() {
       <form name="contact" method="post" data-netlify="true">
         
         <p>
-          <label for="name">Name</label><br />
+          <label htmlFor="name">Name</label><br />
           <input type="text" id="name" name="name" /> 
         </p>
 
         <p>
-          <label for="email">Email</label><br />
+          <label htmlFor="email">Email</label><br />
           <input type="text" id="email" name="email" /> 
         </p>
 
         <p>
-          <label for="messsage">Message</label><br />
+          <label htmlFor="messsage">Message</label><br />
           <textarea id="message" name="message"></textarea> 
         </p>
 
